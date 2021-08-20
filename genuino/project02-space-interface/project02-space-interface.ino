75f82b7008433e2eb293e7d2ddd664f6444d5ced
Jens Brulmans <brulmansjens@gmail.com>
2021-08-20 23:37:34 +0200

    Finish project 02

--- genuino/project02-space-interface/project02-space-interface.ino
+++ genuino/project02-space-interface/project02-space-interface.ino
@@ -1,3 +1,5 @@
+int switchState = 0;
+
 void setup() {
   pinMode(2, INPUT);
   pinMode(3, OUTPUT);
@@ -6,6 +8,20 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  switchState = digitalRead(2);
 
+  if (switchState == LOW) {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+  } else {
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+
+    delay(250);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    delay(250);
+  }
 }
